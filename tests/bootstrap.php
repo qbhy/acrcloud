2a47b422b1fc57a1e1e1dafed81aaa8fbebe0a37
qbhy <qbhy0715@qq.com>
2021-09-26 10:52:57 +0800

    feat: 升级foundation-sdk

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,12 @@
 <?php
 
-use Dotenv\Dotenv;
-use Dotenv\Environment\Adapter\EnvConstAdapter;
-use Dotenv\Environment\Adapter\PutenvAdapter;
-use Dotenv\Environment\Adapter\ServerConstAdapter;
-use Dotenv\Environment\DotenvFactory;
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
-Dotenv::create(
-    __DIR__ . '/..',
-    '.env',
-    new DotenvFactory([new EnvConstAdapter, new PutenvAdapter, new ServerConstAdapter])
-)->load();
+$repository = \Dotenv\Repository\RepositoryBuilder::createWithNoAdapters()
+    ->addAdapter(\Dotenv\Repository\Adapter\EnvConstAdapter::class)
+    ->addWriter(\Dotenv\Repository\Adapter\PutenvAdapter::class)
+    ->immutable()
+    ->make();
 
+$dotenv = Dotenv\Dotenv::create($repository, __DIR__ . '/..');
+$dotenv->load();
